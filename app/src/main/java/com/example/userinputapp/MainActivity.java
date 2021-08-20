234c870734680c8867a7e74a19104d724c6bd077
AashikaLilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-08-20 12:52:48 -0500

    mooore

--- app/src/main/java/com/example/userinputapp/MainActivity.java
+++ app/src/main/java/com/example/userinputapp/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.userinputapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_NAME = "com.example.userinputapp.NAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +38,30 @@ public class MainActivity extends AppCompatActivity {
             String fullName = firstName + " " + lastName;
             TextView fullNameTextView = findViewById(R.id.completeNameTextView);
             fullNameTextView.setText(fullName);
+        }
+        //This method is an example of taking text from an EditText on one screen
+        // and displaying it on a DIFFERENT SCREEN. We are using Intents to switch
+        // activities.
+
+    public void sendMessage(View v)
+        {
+            //this creates the intent that will go from this current Activity to
+            //DisplayWelcomeActivity
+            Intent intent  = new Intent(this, DisplayWelcomeActivity.class);
+            EditText firstNameET = findViewById(R.id.firstNameEditText);
+            EditText lastNameET = findViewById(R.id.lastNameEditText);
+
+            String firstName = firstNameET.getText().toString();
+            String lastName = lastNameET.getText().toString();
+
+            String fullName = firstName + " " + lastName;
+
+            //attaching the full name String to the intent so we can send it alone to the new
+            //Activity
+            intent.putExtra(EXTRA_NAME, fullName);
+
+            //this method will start the new Activity
+            startActivity(intent);
         }
 
 }
